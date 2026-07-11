3c399cb3e6e5bf404a354f1afed588004c9b875d
sumit <sumit9354800@gmail.com>
2026-07-11 19:36:11 +0530

    chore: setup project foundation

---
+++ src/constants/site.ts
@@ -0,0 +1,8 @@
+export const SITE_CONFIG = {
+  name: "MRTECHYCOOL",
+  description: "Professional AC & LED Repair Services",
+  phone: "9528013976",
+  email: "mytechycoolservice@gmail.com",
+  address: "Office number 819 mangla heights app dwarka near palam metro delhi",
+  whatsapp: "9528013976",
+};
