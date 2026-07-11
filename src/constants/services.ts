6e164494a9d1e4ba9861586fa9ecd24efc284f62
sumit <sumit9354800@gmail.com>
2026-07-12 01:02:45 +0530

    service.ts add service

---
+++ src/constants/services.ts
@@ -0,0 +1,8 @@
+export const SERVICES = [
+  "AC Repair",
+  "AC Installation",
+  "Gas Refilling",
+  "LED TV Repair",
+  "Annual Maintenance",
+  "Emergency Service",
+] as const;
---
+++ src/features/contact/contact.data.ts
@@ -0,0 +1,35 @@
+import {
+  MapPin,
+  Phone,
+  Mail,
+  Clock3,
+} from "lucide-react";
+
+export const contactInfo = [
+  {
+    icon: Phone,
+    title: "Call Us",
+    value: "+91 9528013976",
+  },
+  {
+    icon: Phone,
+    title: "Alternate Number",
+    value: "+91 8126102028",
+  },
+  {
+    icon: Mail,
+    title: "Email",
+    value: "mytechycoolservice@gmail.com",
+  },
+  {
+    icon: MapPin,
+    title: "Address",
+    value:
+      "Office No. 819, Mangla Heights App, Near Palam Metro, Dwarka, Delhi",
+  },
+  {
+    icon: Clock3,
+    title: "Working Hours",
+    value: "Mon - Sun | 9:00 AM - 8:00 PM",
+  },
+];
